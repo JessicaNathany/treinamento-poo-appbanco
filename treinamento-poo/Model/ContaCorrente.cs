b11268c20390091f5320c1aaa3b8f15be763dca4
Jéssica Nathany Carvalho Freitas <jnathany@outlook.com>
2021-01-24 16:15:33 -0300

    removendo propriedades

--- treinamento-poo/Model/ContaCorrente.cs
+++ treinamento-poo/Model/ContaCorrente.cs
@@ -2,9 +2,6 @@
 {
     public class ContaCorrente 
     {
-        public string Titular { get; private set; }
-        public int Agencia { get; private set; }
-        public int Numero { get; private set; }
-        public double Saldo { get; private set; }
+       
     }
 }
